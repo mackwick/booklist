ff59e041733e655e0735c0ea7a5a7f6240ff6537
mackwick <mackenziewicker@gmail.com>
2023-12-20 19:45:52 -0500

    got a schema

---
+++ config/db.js
@@ -0,0 +1,17 @@
+//DEPENDENCIES
+
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.DATABASE_URL)
+
+//CONNECTION STATUS LISTENERS
+
+mongoose.connection.on("error", (err) => {
+    console.log(err.message + " oops there is an error!")
+})
+mongoose.connection.on("connected", () => {
+    console.log("connected to mongo")
+})
+mongoose.connection.on("disconnected", () => {
+    console.log("disconnected from mongo")
+})
