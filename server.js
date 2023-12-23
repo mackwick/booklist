78c4cf17c1d249d15cdefcac71311b8f90ab2228
mackwick <mackenziewicker@gmail.com>
2023-12-23 15:50:49 -0500

    self styles

--- server.js
+++ server.js
@@ -1,46 +1,44 @@
-//DEPENDENCIES
+/**
+ * Dependencies
+ */
+require("dotenv").config() // this is how we make use of our .env variables
+require("./config/db") // bring in our db config
 const express = require("express")
-const morgan = require("morgan") // bff w/ all the logger tea
+const morgan = require("morgan") // logger
 const methodOverride = require("method-override")
-const bookRouter = require("./routes/book")
+const bookRouter = require("./routes/book.js");
 
+const app = express();
+const { PORT = 3013 } = process.env; 
 const seedData = require("./models/seed")
-require("dotenv").config() // how we access our .env variables
-require("./config/db") //bring in our db config to run after database is configured
 
-const app = express()
-const {PORT = 3013} = process.env
+// Bring in our model
+const Book = require("./models/Book")
 
-
-// other way of writing above: const PORT = proces.env.PORT || 3013
-
-const Book = require("./models/Book.js")
-
-//MIDDLEWARE
+/**
+ * Middleware
+ */
 app.use((req, res, next) => {
     req.model = {
         Book,
         seedData
     }
     console.log("this is middleware")
+
     // go to the next app method
     next()
 })
-app.use(morgan("dev"))
-app.use(express.urlencoded({extended: true})) //how we access req.body!!!
-app.use(methodOverride("_method")) //lets us use DELETE PUT HTTP verbs
-
-
-
-
-//ROUTES & ROUTER
-//app.use(prefix url, router to execute)
+app.use(morgan("dev")) // logging
+app.use(express.urlencoded({ extended: true })) // body parser this is how we get access to req.body
+app.use(methodOverride("_method")) // Lets us use DELETE PUT HTTP verbs 
+app.use("/public", express.static("public")) // server up our public directory the the url prefix of /public/styles.css
+/**
+ * Routes & Router
+ */
+// app.use(prefix url, router to execute)
 app.use("/books", bookRouter)
 
-
-//SERVER LISTENER
-app.listen(PORT, () => {
-    console.log(`Listening to the sounds of ${PORT}`)
-})
-
-
+/**
+ * Server listener
+ */
+app.listen(PORT, () => console.log(`Listening to the sounds of ${PORT}`))
